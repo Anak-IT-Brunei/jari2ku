97a517a36cab52add3433ada15765c57b262f793
Qoyyuum <abdul.qoyyuum@gmail.com>
2020-04-29 11:09:44 +0800

    Revamp init

---
+++ bsl/src/layouts/MainLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Brunei Sign Language
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer>
+      <q-tabs>
+        <q-tab v-for="nav in navs"
+        :key="nav.link"
+        :label="nav.label" 
+        :icon="nav.icon"
+        />
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink'
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Docs',
+          caption: 'quasar.dev',
+          icon: 'school',
+          link: 'https://quasar.dev'
+        },
+        {
+          title: 'Anak IT',
+          caption: 'www.anak.it',
+          icon: 'code',
+          link: 'https://anak.it'
+        }
+      ],
+      navs: [
+        {
+          label: 'Home',
+          icon: 'home',
+          link: '/'
+        },
+        {
+          label: 'About',
+          icon: 'info',
+          link: '/about'
+        }
+      ]
+    }
+  }
+}
+</script>
--- node_modules/cordova/e2e/fixtures/hooks_bat/test/07.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-ECHO this is script 0 in %~dp0
-ECHO b >> hooks_order.txt
